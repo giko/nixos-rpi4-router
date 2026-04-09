829b7925f11748f045ef9622f8880c930020a5c9
giko <gikon27@proton.me>
2026-04-09 15:51:30 +0300

    Add services module: SSH, chrony

--- modules/services.nix
+++ modules/services.nix
@@ -1,1 +1,34 @@
-{ lib, ... }: { }
+{ config, lib, ... }:
+let
+  cfg = config.router;
+  scfg = cfg.ssh;
+in
+{
+  options.router.ssh = {
+    enable = lib.mkOption { type = lib.types.bool; default = true; };
+    permitRootLogin = lib.mkOption { type = lib.types.bool; default = true; };
+    passwordAuth = lib.mkOption { type = lib.types.bool; default = true; };
+    authorizedKeys = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [];
+      description = "SSH authorized public keys for root.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.openssh = lib.mkIf scfg.enable {
+      enable = true;
+      settings = {
+        PermitRootLogin = if scfg.permitRootLogin then "yes" else "no";
+        PasswordAuthentication = scfg.passwordAuth;
+      };
+    };
+
+    users.users.root.openssh.authorizedKeys.keys = lib.mkIf (scfg.authorizedKeys != []) scfg.authorizedKeys;
+
+    services.chrony.enable = true;
+    services.chrony.extraConfig = ''
+      makestep 1 -1
+    '';
+  };
+}
